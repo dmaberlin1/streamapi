f0fad992031569ad1933d4098bf85136d9471259
dmaberlin1 <dmaberlin77@gmail.com>
2023-10-03 16:12:09 +0300

    feat: task1

--- Stream API/task1/src/Solution.java
+++ Stream API/task1/src/Solution.java
@@ -1,28 +1,43 @@
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
     /**
-    *Найдите все заголовки задач с TaskType READING, и отсортируйте их по дате создания.
-    *Решить необходимо в 1 stream.
-    */
+     * Найдите все заголовки задач с TaskType READING, и отсортируйте их по дате создания.
+     * Решить необходимо в 1 stream.
+     */
 
     public static void main(String[] args) {
-        Task task1 = new Task(1, "Read Version Control with Git book", TaskType.READING, LocalDate.of(2015, Month.JULY, 1)).addTag("git").addTag("reading").addTag("books");
-        Task task2 = new Task(2, "Read Java 8 Lambdas book", TaskType.READING, LocalDate.of(2015, Month.JULY, 2)).addTag("java8").addTag("reading").addTag("books");
-        Task task3 = new Task(3, "Write a mobile application to store my tasks", TaskType.CODING, LocalDate.of(2015, Month.JULY, 3)).addTag("coding").addTag("mobile");
-        Task task4 = new Task(4, "Write a blog on Java 8 Streams", TaskType.WRITING, LocalDate.of(2015, Month.JULY, 4)).addTag("blogging").addTag("writing").addTag("streams");
-        Task task5 = new Task(5, "Read Domain Driven Design book", TaskType.READING, LocalDate.of(2015, Month.JULY, 5)).addTag("ddd").addTag("books").addTag("reading");
+        Task task1 = new Task(1, "Read Version Control with Git book",
+                TaskType.READING, LocalDate.of(2015, Month.JULY, 1)).addTag("git").addTag("reading").addTag("books");
+        Task task2 = new Task(2, "Read Java 8 Lambdas book",
+                TaskType.READING, LocalDate.of(2015, Month.JULY, 2)).addTag("java8").addTag("reading").addTag("books");
+        Task task3 = new Task(3, "Write a mobile application to store my tasks",
+                TaskType.CODING, LocalDate.of(2015, Month.JULY, 3)).addTag("coding").addTag("mobile");
+        Task task4 = new Task(4, "Write a blog on Java 8 Streams",
+                TaskType.WRITING, LocalDate.of(2015, Month.JULY, 4)).addTag("blogging").addTag("writing").addTag("streams");
+        Task task5 = new Task(5, "Read Domain Driven Design book",
+                TaskType.READING, LocalDate.of(2015, Month.JULY, 5)).addTag("ddd").addTag("books").addTag("reading");
         List<Task> tasks = Arrays.asList(task1, task2, task3, task4, task5);
 
         allReadingTasks(tasks).forEach(System.out::println);
     }
 
     private static List<String> allReadingTasks(List<Task> tasks) {
-        return null;
-        // Ваш код здесь
+        return tasks.stream()
+                .filter(task -> task.getType() ==TaskType.READING)
+                .sorted(Comparator.comparing(Task::getCreatedOn))
+                .map(Task::getTitle)
+                .collect(Collectors.toList());
     }
+    //    Функция map применяет указанную функцию к каждому элементу потока, создавая новый поток с преобразованными данными.
+//    Преобразованные заголовки задач собираются обратно в список (List) с использованием Collectors.toList().
+//    метод завершает операцию потока и возвращает список строк, содержащий заголовки всех задач READING, отсортированных по дате создания.
+
+
 }
